836b27042133e4211c5804c07134ccfd7b4c85d5
June Wu <s414june@gmail.com>
2024-02-29 15:39:39 +0800

    add firebase test

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>消費類型</th>
+        <th>金額</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script setup lang="ts">
+import { onMounted,ref } from "vue";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCw7LLj6ZQohgmlDtV9i4_42o5hJMjg-Z0",
+  authDomain: "accounting-online-8cbe5.firebaseapp.com",
+  projectId: "accounting-online-8cbe5",
+  storageBucket: "accounting-online-8cbe5.appspot.com",
+  messagingSenderId: "1050802574579",
+  appId: "1:1050802574579:web:7b427219663f5a80aa5386",
+  measurementId: "G-09TJEN4W5S",
+};
+
+const spending = ref([{
+    type:Number,
+    count:Number
+}])
+onMounted(async () => {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+
+  const spendingSnap = await getDoc(doc(db, "amounting", "spending"));
+  console.log(spendingSnap.data());
+//   spending.value = spendingSnap.data()
+});
+</script>
