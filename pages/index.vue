38c0b1db67808fa321b4955a99f75a5467966a84
June Wu <s414june@gmail.com>
2024-03-01 18:38:07 +0800

    add tailwind, el plus

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,32 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>消費類型</th>
-        <th>金額</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-      </tr>
-    </tbody>
-  </table>
+  <el-container class="w-full items-center">
+    <el-header>
+      <h1>別再花</h1>
+    </el-header>
+    <el-main>
+      <el-button round class="w-56">
+        <el-image src="/img/google.svg" class="mr-2 w-4" />
+        以Google帳號登入
+      </el-button>
+      <table>
+        <thead>
+          <tr>
+            <th>消費類型</th>
+            <th>金額</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </el-main>
+  </el-container>
 </template>
 <script setup lang="ts">
-import { onMounted,ref } from "vue";
+import { onMounted, ref } from "vue";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -36,10 +47,12 @@ const firebaseConfig = {
   measurementId: "G-09TJEN4W5S",
 };
 
-const spending = ref([{
-    type:Number,
-    count:Number
-}])
+const spending = ref([
+  {
+    type: Number,
+    count: Number,
+  },
+]);
 onMounted(async () => {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
@@ -48,6 +61,6 @@ onMounted(async () => {
 
   const spendingSnap = await getDoc(doc(db, "amounting", "spending"));
   console.log(spendingSnap.data());
-//   spending.value = spendingSnap.data()
+  //   spending.value = spendingSnap.data()
 });
 </script>
